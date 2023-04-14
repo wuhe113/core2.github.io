ee6b67b4afa76866347cb18d54ef1a24a74cdccf
Helen Wu <wuz420@newschool.edu>
2023-04-14 10:09:04 -0400

    Fetched data

--- week12_lab/assets/data.js
+++ week12_lab/assets/data.js
@@ -1,1 +1,7 @@
-console.log("JS is connected.")
+const dataUrl = 'https://data.cityofnewyork.us/resource/vfnx-vebw.json'
+
+fetch(dataUrl + '?$limit=5000')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+    })
