39abfad40797dc3a260be394ccac7a2dca47cb56
Helen Wu <wuz420@newschool.edu>
2023-04-14 10:51:30 -0400

    Added parse function and counting squirrel color(gray)

--- week12_lab/assets/data.js
+++ week12_lab/assets/data.js
@@ -1,7 +1,28 @@
 const dataUrl = 'https://data.cityofnewyork.us/resource/vfnx-vebw.json'
 
-fetch(dataUrl + '?$limit=5000')
+const parseData = (squirrels) => {
+    let colorGray = 0
+    let colorCinnamon = 0
+    let colorBlack = 0
+    let colorUnknown = 0
+
+    squirrels.forEach(squirrel => {
+        // console.log(squirrel.primary_fur_color)
+        // if (squirrel.primary_fur_color == 'Gray') console.log("This is a gray squirrel")
+        // else if (squirrel.primary_fur_color == 'Cinnamon') console.log("This is a red")
+        // else console.log("This is an unknown squirrel color")
+
+        if (squirrel.primary_fur_color == 'Gray') colorGray = colorGray + 1
+
+    })
+
+    console.log('Gray:' + colorGray)
+
+    // console.log(data)
+}
+
+fetch(dataUrl + '?$limit=15')
     .then(response => response.json())
     .then(data => {
-        console.log(data)
+        parseData(data)
     })
